dacf88b26f39bd897153426b62a81d1b466745d5
hardfloor <62759083+hardfloor@users.noreply.github.com>
2020-09-18 09:20:21 +0200

    maj2

--- mail.php
+++ mail.php
@@ -2,29 +2,52 @@
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+
 require './PHPMailer-6.1.7/src/Exception.php';
 require './PHPMailer-6.1.7/src/PHPMailer.php';
 require './PHPMailer-6.1.7/src/SMTP.php';
+
 $mail = new PHPMailer(true);
+
 try {
-$mail->SMTPDebug = 0;
-$mail->isSMTP();
-$mail->Host = 'smtp-relay.sendinblue.com';
-$mail->SMTPAuth = true;
-$mail->Username = 'jean-sebastien.metayer@orange.fr';
-$mail->Password = 'dwHDAGtq8NmC0BYx';
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-$mail->setFrom('john_doe@gmail.com', 'John Doe');
-$mail->addAddress($_POST["email"]);
-$mail->isHTML(true);
-$mail->Name = $_POST["name"]
-$mail->Subject = $_POST["subject"];
-$mail->Body = $_POST["message"];
-$mail->AltBody = 'Mail au format texte';
-$mail->send();
-echo 'Message has been sent';
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp-relay.sendinblue.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'jean-sebastien.metayer@orange.fr';
+    $mail->Password   = 'xsmtpsib-9bcbd93c1b7ebf8525570d675960615475e9cacfead94905ad8b8c9277cfa2e5-XMt3vR6jgB8T4zUH';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    $mail->setFrom($_POST["email"], "");
+    $mail->addAddress("jean-sebastien.metayer@orange.fr");
+
+    $mail->isHTML(true);
+    $mail->Subject = $_POST["subject"];
+    $mail->Body    = $_POST["message"];
+    $mail->AltBody = $_POST["message"];
+
+    $mail->send();
+
+    // $mail->SMTPDebug = 0;
+    // $mail->isSMTP();
+    // $mail->Host = 'smtp-relay.sendinblue.com';
+    // $mail->SMTPAuth = true;
+    // $mail->Username = 'jean-sebastien.metayer@orange.fr';
+    // $mail->Password = 'dwHDAGtq8NmC0BYx';
+    // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    // $mail->Port = 587;
+    // $mail->setFrom('john_doe@gmail.com', 'John Doe');
+    // $mail->addAddress($_POST["email"]);
+    // $mail->isHTML(true);
+    // $mail->Name = $_POST["name"]
+    // $mail->Subject = $_POST["subject"];
+    // $mail->Body    = $_POST["message"];
+    // $mail->AltBody = $_POST["message"];
+    // $mail->send();
+    
+    echo 'Votre message a bien été envoyé!';
 } catch (Exception $e) {
-echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Désolé, votre message n'a pas pu etre envoyé, merci d'essayer à nouveau: {$mail->ErrorInfo}";
 }
 ?>
